a96b73f10ed8bf627e0416c6ed8a433a2ed54afe
mateus <mateusadc19@gmail.com>
2022-04-08 22:11:27 -0300

    Finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿ int numero_digitado;
+
+// Pede para o Usúario Digitar um número 
+Console.Write("Digite um Número: ");
+
+//Armazena o Número Digitado em uma Variavel  
+numero_digitado = Convert.ToInt32 (Console.ReadLine());
+
+bool paridade = (numero_digitado % 2) == 0;
+
+if (paridade)
+{
+    Console.WriteLine("Par"); 
+}         
+else
+{
+    Console.WriteLine("Impar");  
+}
+
